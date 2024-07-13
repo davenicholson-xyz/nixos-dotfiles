4a35d1c6a1832b4417249f23f4167d1fbb023122
Dave Nicholson <me@davenicholson.xyz>
2024-07-13 13:36:05 +0100

    long args and safe input

--- scripts/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Run this once NixOS has been built and switched for the first time
-
-# Make symlink for dunst config from pywal generated config
-ln -s $HOME/.cache/wal/dunstrc $HOME/.config/dunst/dunstrc
